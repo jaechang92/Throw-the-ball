4654de31ddbfe1ddb56912928bd49bc39baa134a
jaechang <jaechang0721k@gmail.com>
2020-04-28 14:45:40 +0900

    Ball

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -22,6 +22,7 @@ public class BallController : MonoBehaviour
     public Transform m_Tr;
     [Tooltip("KM/H")]
     public float m_StartSpeed;
+    private float speed;
     public float RPM;
     public float m_ForceOfStartRotation;
 
@@ -44,6 +45,35 @@ public class BallController : MonoBehaviour
 
     public int count = 0;
     public Vector3 angleV;
+
+    public bool isStart = false;
+
+    public Vector3 originPos;
+    public Quaternion originRot;
+
+    public void Init()
+    {
+        angleSpeed = RPM / 60 * Mathf.PI * 2;
+        if (forwardRotation)
+        {
+            angleSpeed = -angleSpeed;
+        }
+
+        speed = m_StartSpeed * 10 / 36;
+        m_rb.velocity = targetTr.normalized * speed;
+        m_rb.angularVelocity = spinAngle * angleSpeed;
+        // x 축회전은 상하
+        // y 축회전은 전후
+        // z 축회전은 좌우
+
+        //m_rb.AddTorque(Vector3.left * m_StartSpin);
+        //m_rb.AddForce(Vector3.forward * m_StartSpeed);
+        m_rb.maxAngularVelocity = 1000000;
+
+        time = 0;
+
+    }
+
     void Awake()
     {
         m_Tr = GetComponent<Transform>();
@@ -59,43 +89,26 @@ public class BallController : MonoBehaviour
         volume = Mathf.PI * 4 / 3 * radius * radius * radius;
         //constNum = 1;
 
-        angleSpeed = RPM / 60 * Mathf.PI * 2;
-
+        originPos = this.gameObject.transform.position;
+        originRot = this.gameObject.transform.rotation;
     }
 
-    void Start()
-    {
-        if (forwardRotation)
-        {
-            angleSpeed = -angleSpeed;
-        }
-
-
-        //시속 100km/h 이면 100000m/h 이고 1000m/ 36s
-        m_StartSpeed = m_StartSpeed * 10 / 36;
-        m_rb.velocity = targetTr.normalized * m_StartSpeed;
-        m_rb.angularVelocity = spinAngle * angleSpeed;
-
-
-        // x 축회전은 상하
-        // y 축회전은 전후
-        // z 축회전은 좌우
-
-        //m_rb.AddTorque(Vector3.left * m_StartSpin);
-        //m_rb.AddForce(Vector3.forward * m_StartSpeed);
-        m_rb.maxAngularVelocity = 1000000;
-    }
 
 
     void Update()
     {
         time += Time.deltaTime;
         //Debug.Log(magnusForce);
+        CreateCircle();
+
 
     }
 
     void FixedUpdate()
     {
+
+        if (isStart)
+        {
         //angleSpeed = Mathf.Sqrt(Mathf.Pow(m_rb.angularVelocity.x, 2) + Mathf.Pow(m_rb.angularVelocity.y, 2) + Mathf.Pow(m_rb.angularVelocity.z, 2));
         
         foreceDirection = m_Tr.TransformVector(new Vector3(m_rb.angularVelocity.y, -m_rb.angularVelocity.x, m_rb.angularVelocity.z));
@@ -112,8 +125,7 @@ public class BallController : MonoBehaviour
         m_rb.AddForce(foreceDirection * magnusForce);
 
         angleV = m_rb.angularVelocity;
-        
-
+        }
 
     }
 
@@ -137,6 +149,46 @@ public class BallController : MonoBehaviour
         Debug.Log(count);
     }
     
+    public void StartBtn()
+    {
+        Init();
+        isStart = true;
+    }
 
-    
+    public void ResetBtn()
+    {
+        isStart = false;
+        m_Tr.position = originPos;
+        m_Tr.rotation = originRot;
+
+        m_rb.velocity = Vector3.zero;
+        m_rb.angularVelocity = Vector3.zero;
+        checkZ = 0;
+
+        foreach (var item in objPool)
+        {
+            Destroy(item);
+        }
+        objPool.Clear();
+    }
+
+
+    public GameObject prefab;
+    private float checkZ = 0;
+    public List<GameObject> objPool;
+    public void CreateCircle()
+    {
+        if (m_Tr.position.z > checkZ)
+        {
+            GameObject obj= Instantiate(prefab, m_Tr.position,prefab.transform.rotation);
+            obj.transform.position = new Vector3 (0, obj.transform.position.y, obj.transform.position.z);
+            objPool.Add(obj);
+            checkZ++;
+        }
+
+        
+    }
+       
+
+
 }
